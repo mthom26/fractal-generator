5fbd5417b38e45ee064646082b6a74384cecbcee
mthom26 <mthom2626@gmail.com>
2019-04-27 16:05:32 +0100

    Add 'image' crate and output image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use image;
+
 fn main() {
-    println!("Hello, world!");
+    let img_width = 1000;
+    let img_height = 1000;
+
+    let mut img_buffer = image::ImageBuffer::new(img_width, img_height);
+
+    // Fill image background
+    for (_, _, pixel) in img_buffer.enumerate_pixels_mut() {
+        *pixel = image::Rgb([25, 25, 25]);
+    }
+
+    img_buffer.save("output.jpeg").unwrap();
 }
