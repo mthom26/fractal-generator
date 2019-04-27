eeebcb27f5c5a9dd2f49edaaa2813180681e75ef
mthom26 <mthom2626@gmail.com>
2019-04-27 16:54:04 +0100

    Add 'num_complex' crate and generate a fractal

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use image;
+use num_complex;
 
 fn main() {
     let img_width = 1000;
     let img_height = 1000;
+
+    let scale_x = 4.0 / img_width as f32;
+    let scale_y = 4.0 / img_height as f32;
 
     let mut img_buffer = image::ImageBuffer::new(img_width, img_height);
 
@@ -11,5 +15,27 @@ fn main() {
         *pixel = image::Rgb([25, 25, 25]);
     }
 
-    img_buffer.save("output.jpeg").unwrap();
+    // Constant complex number to use for each iteration
+    let c = num_complex::Complex::new(-0.4, 0.6);
+
+    for x in 0..img_width {
+        for y in 0..img_height {
+
+            let zx = y as f32 * scale_x - 2.0;
+            let zy = x as f32 * scale_y - 2.0;
+
+            let mut z = num_complex::Complex::new(zx, zy);
+
+            let mut i = 0;
+            while i < 255 && z.norm() <= 2.0 {
+                z = z * z + c;
+                i += 1;
+            }
+
+            let pixel = img_buffer.get_pixel_mut(x, y);
+            *pixel = image::Rgb([25, i, 25]);
+        }
+    }
+
+    img_buffer.save("output.png").unwrap();
 }
