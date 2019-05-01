a37dfcbd885c26f233f45e23c38bd4f207259b26
mthom26 <mthom2626@gmail.com>
2019-05-01 19:47:35 +0100

    Multi thread pattern building and file output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 use image;
 use indicatif::{ProgressBar, ProgressStyle};
 use num_complex;
@@ -21,43 +23,44 @@ fn main() {
     let scale_x = scale * aspect_ratio / img_width as f32;
     let scale_y = scale / img_height as f32;
 
-    let mut img_buffer = image::ImageBuffer::new(img_width, img_height);
+    let mut workers = Vec::new();
+    for num in cfg.complex_num.iter() {
+        let mut img_buffer = image::ImageBuffer::new(img_width, img_height);
 
-    // Fill image background
-    for (_, _, pixel) in img_buffer.enumerate_pixels_mut() {
-        *pixel = image::Rgb([25, 25, 25]);
-    }
-
-    // Constant complex number to use for each iteration
-    let c = num_complex::Complex::new(cfg.complex_num[0].0, cfg.complex_num[0].1);
-
-    println!("Building pattern...");
-    let progress = ProgressBar::new(img_width as u64);
-    progress.set_style(
-        ProgressStyle::default_bar()
-            .template("{bar:80.green.dim/white.dim} {pos:>4}/{len} [{elapsed}]"),
-    );
-
-    for x in 0..img_width {
-        for y in 0..img_height {
-            let zx = y as f32 * scale_x - offset_x;
-            let zy = x as f32 * scale_y - offset_y;
-
-            let mut z = num_complex::Complex::new(zx, zy);
-
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z + c;
-                i += 1;
-            }
-
-            let pixel = img_buffer.get_pixel_mut(x, y);
-            *pixel = image::Rgb([25, i, 25]);
+        // Fill image background
+        for (_, _, pixel) in img_buffer.enumerate_pixels_mut() {
+            *pixel = image::Rgb([25, 25, 25]);
         }
-        progress.inc(1);
-    }
-    progress.finish();
+        let c = num_complex::Complex::new(num.0, num.1);
 
-    let filename = format!("output_{}_{}i.png", c.re, c.im);
-    img_buffer.save(filename).unwrap();
+        let worker = thread::spawn(move || {
+            for x in 0..img_width {
+                for y in 0..img_height {
+                    let zx = y as f32 * scale_x - offset_x;
+                    let zy = x as f32 * scale_y - offset_y;
+
+                    let mut z = num_complex::Complex::new(zx, zy);
+
+                    let mut i = 0;
+                    while i < 255 && z.norm() <= 2.0 {
+                        z = z * z + c;
+                        i += 1;
+                    }
+
+                    let pixel = img_buffer.get_pixel_mut(x, y);
+                    *pixel = image::Rgb([25, i, 25]);
+                }
+            }
+            // TODO - Make sure no two files have the same name 
+            // for example two identical complex numbers are passed
+            let filename = format!("output_{}_{}i.png", c.re, c.im);
+            img_buffer.save(filename).unwrap();
+        });
+        workers.push(worker);
+    }
+
+    // Wait for threads to finish working
+    for t in workers.into_iter() {
+        t.join().unwrap();
+    }
 }
